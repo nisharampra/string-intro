3d4a05b79fbb4c977b36aba9fcd14b4c3ea8b83f
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 22:59:56 +0800

    Add files via upload

---
+++ string intro/string intro/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  string intro
+//
+//  Created by Nisha Ramprasath on 1/11/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+//    \o is a null statemnt
+    char s[]={'h','e','l','l','o','\0'};
+    cout<<s<<endl;
+    
+    
+ return 0;
+}
